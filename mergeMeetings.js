cb16ed3c4d201b84731710c02b20df5ca70a2b70
Anthony Lee <ahjlee74@gmail.com>
2021-04-19 17:09:53 -0700

    reverse 4/19/2021

---
+++ mergeMeetings.js
@@ -0,0 +1,58 @@
+function mergeRanges(meetings) {
+
+  // Merge meetings ranges
+
+  // Create a deep copy of the meetings array
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Deep_Clone
+  const meetingsCopy = [...meetings];
+
+  // Sort by start time
+  const sortedMeetings = meetingsCopy.sort((a, b) => {
+    return a.startTime - b.startTime;
+  });
+
+  // Initialize mergedMeetings with the earliest meeting
+  const mergedMeetings = [sortedMeetings[0]];
+
+  for (let i = 1; i < sortedMeetings.length; i++) {
+    const currentMeeting    = sortedMeetings[i];
+    const lastMergedMeeting = mergedMeetings[mergedMeetings.length - 1];
+
+    // If the current meeting overlaps with the last merged meeting, use the
+    // later end time of the two
+    if (currentMeeting.startTime <= lastMergedMeeting.endTime) {
+      lastMergedMeeting.endTime = Math.max(lastMergedMeeting.endTime, currentMeeting.endTime);
+    } else {
+
+      // Add the current meeting since it doesn't overlap
+      mergedMeetings.push(currentMeeting);
+    }
+  }
+
+  return mergedMeetings;
+}
+
+//python
+
+def merge_ranges(meetings):
+
+    # Sort by start time
+    sorted_meetings = sorted(meetings)
+
+    # Initialize merged_meetings with the earliest meeting
+    merged_meetings = [sorted_meetings[0]]
+
+    for current_meeting_start, current_meeting_end in sorted_meetings[1:]:
+        last_merged_meeting_start, last_merged_meeting_end = merged_meetings[-1]
+
+        # If the current meeting overlaps with the last merged meeting, use the
+        # later end time of the two
+        if (current_meeting_start <= last_merged_meeting_end):
+            merged_meetings[-1] = (last_merged_meeting_start,
+                                   max(last_merged_meeting_end,
+                                       current_meeting_end))
+        else:
+            # Add the current meeting since it doesn't overlap
+            merged_meetings.append((current_meeting_start, current_meeting_end))
+
+    return merged_meetings
