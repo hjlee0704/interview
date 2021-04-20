bafc362914410bba62a4fb4910302e667d8f5de5
Anthony Lee <ahjlee74@gmail.com>
2021-04-20 16:44:48 -0700

    420/2021

---
+++ balancedTree.js
@@ -0,0 +1,211 @@
+
+
+
+
+
+
+
+
+// Tests
+
+let desc = 'full tree';
+let treeRoot = new BinaryTreeNode(5);
+let leftNode = treeRoot.insertLeft(8);
+leftNode.insertLeft(1);
+leftNode.insertRight(2);
+let rightNode = treeRoot.insertRight(6);
+rightNode.insertLeft(3);
+rightNode.insertRight(4);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'both leaves at the same depth';
+treeRoot = new BinaryTreeNode(3);
+leftNode = treeRoot.insertLeft(4);
+leftNode.insertLeft(1);
+rightNode = treeRoot.insertRight(6);
+rightNode.insertRight(9);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'leaf heights differ by one';
+treeRoot = new BinaryTreeNode(6);
+leftNode = treeRoot.insertLeft(1);
+rightNode = treeRoot.insertRight(0);
+rightNode.insertRight(7);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'leaf heights differ by two';
+treeRoot = new BinaryTreeNode(6);
+leftNode = treeRoot.insertLeft(1);
+rightNode = treeRoot.insertRight(0);
+rightNode.insertRight(7).insertRight(8);
+assertEquals(isBalanced(treeRoot), false, desc);
+
+desc = 'three leaves total';
+treeRoot = new BinaryTreeNode(1);
+leftNode = treeRoot.insertLeft(5);
+rightNode = treeRoot.insertRight(9);
+rightNode.insertLeft(8);
+rightNode.insertRight(5);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'both subtrees superbalanced';
+treeRoot = new BinaryTreeNode(1);
+leftNode = treeRoot.insertLeft(5);
+rightNode = treeRoot.insertRight(9);
+rightNode.insertLeft(8).insertLeft(7);
+rightNode.insertRight(5);
+assertEquals(isBalanced(treeRoot), false, desc);
+
+desc = 'both subtrees superbalanced two';
+treeRoot = new BinaryTreeNode(1);
+leftNode = treeRoot.insertLeft(2);
+leftNode.insertLeft(3);
+leftNode.insertRight(7).insertRight(8);
+treeRoot.insertRight(4).insertRight(5).insertRight(6).insertRight(9);
+assertEquals(isBalanced(treeRoot), false, desc);
+
+desc = 'three leaves at different levels';
+treeRoot = new BinaryTreeNode(1);
+leftNode = treeRoot.insertLeft(2);
+leftLeft = leftNode.insertLeft(3);
+leftNode.insertRight(4);
+leftLeft.insertLeft(5);
+leftLeft.insertRight(6);
+treeRoot.insertRight(7).insertRight(8).insertRight(9).insertRight(10);
+assertEquals(isBalanced(treeRoot), false, desc);
+
+desc = 'only one node';
+treeRoot = new BinaryTreeNode(1);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'linked list tree';
+treeRoot = new BinaryTreeNode(1);
+treeRoot.insertRight(2).insertRight(3).insertRight(4);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+function assertEquals(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`)
+  }
+}
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    class BinaryTreeNode(object):
+
+        def __init__(self, value):
+            self.value = value
+            self.left = None
+            self.right = None
+
+        def insert_left(self, value):
+            self.left = Test.BinaryTreeNode(value)
+            return self.left
+
+        def insert_right(self, value):
+            self.right = Test.BinaryTreeNode(value)
+            return self.right
+
+    def test_full_tree(self):
+        tree = Test.BinaryTreeNode(5)
+        left = tree.insert_left(8)
+        right = tree.insert_right(6)
+        left.insert_left(1)
+        left.insert_right(2)
+        right.insert_left(3)
+        right.insert_right(4)
+        result = is_balanced(tree)
+        self.assertTrue(result)
+
+    def test_both_leaves_at_the_same_depth(self):
+        tree = Test.BinaryTreeNode(3)
+        left = tree.insert_left(4)
+        right = tree.insert_right(2)
+        left.insert_left(1)
+        right.insert_right(9)
+        result = is_balanced(tree)
+        self.assertTrue(result)
+
+    def test_leaf_heights_differ_by_one(self):
+        tree = Test.BinaryTreeNode(6)
+        left = tree.insert_left(1)
+        right = tree.insert_right(0)
+        right.insert_right(7)
+        result = is_balanced(tree)
+        self.assertTrue(result)
+
+    def test_leaf_heights_differ_by_two(self):
+        tree = Test.BinaryTreeNode(6)
+        left = tree.insert_left(1)
+        right = tree.insert_right(0)
+        right_right = right.insert_right(7)
+        right_right.insert_right(8)
+        result = is_balanced(tree)
+        self.assertFalse(result)
+
+    def test_three_leaves_total(self):
+        tree = Test.BinaryTreeNode(1)
+        left = tree.insert_left(5)
+        right = tree.insert_right(9)
+        right.insert_left(8)
+        right.insert_right(5)
+        result = is_balanced(tree)
+        self.assertTrue(result)
+
+    def test_both_subtrees_superbalanced(self):
+        tree = Test.BinaryTreeNode(1)
+        left = tree.insert_left(5)
+        right = tree.insert_right(9)
+        right_left = right.insert_left(8)
+        right.insert_right(5)
+        right_left.insert_left(7)
+        result = is_balanced(tree)
+        self.assertFalse(result)
+
+    def test_both_subtrees_superbalanced_two(self):
+        tree = Test.BinaryTreeNode(1)
+        left = tree.insert_left(2)
+        right = tree.insert_right(4)
+        left.insert_left(3)
+        left_right = left.insert_right(7)
+        left_right.insert_right(8)
+        right_right = right.insert_right(5)
+        right_right_right = right_right.insert_right(6)
+        right_right_right.insert_right(9)
+        result = is_balanced(tree)
+        self.assertFalse(result)
+
+    def test_three_leaves_at_different_levels(self):
+        tree = Test.BinaryTreeNode(1)
+        left = tree.insert_left(2)
+        left_left = left.insert_left(3)
+        left.insert_right(4)
+        left_left.insert_left(5)
+        left_left.insert_right(6)
+        right = tree.insert_right(7)
+        right_right = right.insert_right(8)
+        right_right_right = right_right.insert_right(9)
+        right_right_right.insert_right(10)
+        result = is_balanced(tree)
+        self.assertFalse(result)
+
+    def test_only_one_node(self):
+        tree = Test.BinaryTreeNode(1)
+        result = is_balanced(tree)
+        self.assertTrue(result)
+
+    def test_linked_list_tree(self):
+        tree = Test.BinaryTreeNode(1)
+        right = tree.insert_right(2)
+        right_right = right.insert_right(3)
+        right_right.insert_right(4)
+        result = is_balanced(tree)
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
---
+++ mergeArray.js
@@ -0,0 +1,142 @@
+function mergeArrays(myArray, alicesArray) {
+
+  // Set up our mergedArray
+  const mergedArray = [];
+
+  let currentIndexAlices = 0;
+  let currentIndexMine = 0;
+  let currentIndexMerged = 0;
+
+  while (currentIndexMerged < (myArray.length + alicesArray.length)) {
+
+    const isMyArrayExhausted = currentIndexMine >= myArray.length;
+    const isAlicesArrayExhausted = currentIndexAlices >= alicesArray.length;
+
+    // Case: next comes from my array
+    // My array must not be exhausted, and EITHER:
+    // 1) Alice's array IS exhausted, or
+    // 2) The current element in my array is less
+    //    than the current element in Alice's array
+    if (!isMyArrayExhausted && (isAlicesArrayExhausted ||
+      (myArray[currentIndexMine] < alicesArray[currentIndexAlices]))) {
+
+      mergedArray[currentIndexMerged] = myArray[currentIndexMine];
+      currentIndexMine++;
+
+      // Case: next comes from Alice's array
+    } else {
+      mergedArray[currentIndexMerged] = alicesArray[currentIndexAlices];
+      currentIndexAlices++;
+    }
+
+    currentIndexMerged++;
+  }
+
+  return mergedArray;
+}
+
+
+// Tests
+
+let desc = 'both arrays are empty';
+let actual = mergeArrays([], []);
+let expected = [];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'first array is empty';
+actual = mergeArrays([], [1, 2, 3]);
+expected = [1, 2, 3];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'second array is empty';
+actual = mergeArrays([5, 6, 7], []);
+expected = [5, 6, 7];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'both arrays have some numbers';
+actual = mergeArrays([2, 4, 6], [1, 3, 7]);
+expected = [1, 2, 3, 4, 6, 7];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'arrays are different lengths';
+actual = mergeArrays([2, 4, 6, 8], [1, 7]);
+expected = [1, 2, 4, 6, 7, 8];
+assertDeepEqual(actual, expected, desc);
+
+function assertDeepEqual(a, b, desc) {
+  const aStr = JSON.stringify(a);
+  const bStr = JSON.stringify(b);
+  if (aStr !== bStr) {
+    console.log(`${desc} ... FAIL: ${aStr} != ${bStr}`);
+  } else {
+    console.log(`${desc} ... PASS`);
+  }
+}
+
+//python
+
+def merge_lists(my_list, alices_list):
+    # Set up our merged_list
+    merged_list_size = len(my_list) + len(alices_list)
+    merged_list = [None] * merged_list_size
+
+    current_index_alices = 0
+    current_index_mine = 0
+    current_index_merged = 0
+    while current_index_merged < merged_list_size:
+        is_my_list_exhausted = current_index_mine >= len(my_list)
+        is_alices_list_exhausted = current_index_alices >= len(alices_list)
+        if (not is_my_list_exhausted and
+                (is_alices_list_exhausted or
+                 my_list[current_index_mine] < alices_list[current_index_alices])):
+            # Case: next comes from my list
+            # My list must not be exhausted, and EITHER:
+            # 1) Alice's list IS exhausted, or
+            # 2) the current element in my list is less
+            #    than the current element in Alice's list
+            merged_list[current_index_merged] = my_list[current_index_mine]
+            current_index_mine += 1
+        else:
+            # Case: next comes from Alice's list
+            merged_list[current_index_merged] = alices_list[current_index_alices]
+            current_index_alices += 1
+
+        current_index_merged += 1
+
+    return merged_list
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_both_lists_are_empty(self):
+        actual = merge_lists([], [])
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_first_list_is_empty(self):
+        actual = merge_lists([], [1, 2, 3])
+        expected = [1, 2, 3]
+        self.assertEqual(actual, expected)
+
+    def test_second_list_is_empty(self):
+        actual = merge_lists([5, 6, 7], [])
+        expected = [5, 6, 7]
+        self.assertEqual(actual, expected)
+
+    def test_both_lists_have_some_numbers(self):
+        actual = merge_lists([2, 4, 6], [1, 3, 7])
+        expected = [1, 2, 3, 4, 6, 7]
+        self.assertEqual(actual, expected)
+
+    def test_lists_are_different_lengths(self):
+        actual = merge_lists([2, 4, 6, 8], [1, 7])
+        expected = [1, 2, 4, 6, 7, 8]
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
