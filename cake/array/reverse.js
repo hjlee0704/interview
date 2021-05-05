0933de9ee5306432df268d14ed2325d7bf6bbb39
Anthony Lee <ahjlee74@gmail.com>
2021-05-05 15:44:55 -0700

    array

---
+++ cake/array/reverse.js
@@ -0,0 +1,51 @@
+function reverse(arrayOfChars) {
+
+  let leftIndex = 0;
+  let rightIndex = arrayOfChars.length - 1;
+
+  while (leftIndex < rightIndex) {
+
+    // Swap characters
+    const temp = arrayOfChars[leftIndex];
+    arrayOfChars[leftIndex] = arrayOfChars[rightIndex];
+    arrayOfChars[rightIndex] = temp;
+
+    // Move towards middle
+    leftIndex++;
+    rightIndex--;
+  }
+}
+
+
+
+
+// Tests
+
+let desc = 'empty string';
+let input = ''.split('');
+reverse(input);
+let actual = input.join('');
+let expected = '';
+assertEqual(actual, expected, desc);
+
+desc = 'single character string';
+input = 'A'.split('');
+reverse(input);
+actual = input.join('');
+expected = 'A';
+assertEqual(actual, expected, desc);
+
+desc = 'longer string';
+input = 'ABCDE'.split('');
+reverse(input);
+actual = reverse(input).join('');
+expected = 'EDCBA';
+assertEqual(actual, expected, desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
