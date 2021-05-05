2fd2e4e3d71eb413a6f7659a5ffb54d70f9c5a5f
Anthony Lee <ahjlee74@gmail.com>
2021-05-05 16:01:22 -0700

    dynamic

---
+++ cake/dynamic/makeChange.js
@@ -0,0 +1,60 @@
+function changePossibilitiesBottomUp(amount, denominations) {
+
+  // Initialize an array of zeros with indices up to amount
+  const waysOfDoingNcents = new Array(amount + 1).fill(0);
+  waysOfDoingNcents[0] = 1;
+
+  denominations.forEach(coin => {
+    for (let higherAmount = coin; higherAmount <= amount; higherAmount++) {
+      const higherAmountRemainder = higherAmount - coin;
+      waysOfDoingNcents[higherAmount] += waysOfDoingNcents[higherAmountRemainder];
+    }
+  });
+
+  return waysOfDoingNcents[amount];
+}
+
+
+
+
+
+
+// Tests
+
+let desc = 'sample input';
+let actual = changePossibilities(4, [1, 2, 3]);
+let expected = 4;
+assertEqual(actual, expected, desc);
+
+desc = 'one way to make zero cents';
+actual = changePossibilities(0, [1, 2]);
+expected = 1;
+assertEqual(actual, expected, desc);
+
+desc = 'no ways if no coins';
+actual = changePossibilities(1, []);
+expected = 0;
+assertEqual(actual, expected, desc);
+
+desc = 'big coin value';
+actual = changePossibilities(5, [25, 50]);
+expected = 0;
+assertEqual(actual, expected, desc);
+
+desc = 'big target amount';
+actual = changePossibilities(50, [5, 10]);
+expected = 6;
+assertEqual(actual, expected, desc);
+
+desc = 'change for one dollar';
+actual = changePossibilities(100, [1, 5, 10, 25, 50]);
+expected = 292;
+assertEqual(actual, expected, desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`)
+  }
+}
