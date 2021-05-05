49a8bb60e0bbe184a5110014e0fc7b37d2984ea9
Anthony Lee <ahjlee74@gmail.com>
2021-05-04 17:45:14 -0700

    3

--- lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring.js
@@ -13,3 +13,18 @@ var lengthOfLongestSubstring = function(s) {
    }
    return res
 };
+
+//optimized
+
+var lengthOfLongestSubstring = function(s) {
+  let res = 0
+  let map = new Map()
+  for(let i=0, j=0; j<s.length; j++){
+      if(map.has(s[j])){
+          i = Math.max(i, map.get(s[j]))
+      }
+      res = Math.max(res, j-i+1)
+      map.set(s[j],j+1)
+  }
+  return res
+};
