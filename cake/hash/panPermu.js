83aec70089a7b2974530445d57623bb24bcc2569
Anthony Lee <ahjlee74@gmail.com>
2021-05-05 15:47:14 -0700

    hash

---
+++ cake/hash/panPermu.js
@@ -0,0 +1,48 @@
+function hasPalindromePermutation(theString) {
+
+  // Track characters we've seen an odd number of times
+  const unpairedCharacters = new Set();
+
+  for (let char of theString) {
+    if (unpairedCharacters.has(char)) {
+      unpairedCharacters.delete(char);
+    } else {
+      unpairedCharacters.add(char);
+    }
+  }
+
+  // The string has a palindrome permutation if it
+  // has one or zero characters without a pair
+  return unpairedCharacters.size <= 1;
+}
+
+
+
+
+// Tests
+
+let desc = 'permutation with odd number of chars';
+assertEqual(hasPalindromePermutation('aabcbcd'), true, desc);
+
+desc = 'permutation with even number of chars';
+assertEqual(hasPalindromePermutation('aabccbdd'), true, desc);
+
+desc = 'no permutation with odd number of chars';
+assertEqual(hasPalindromePermutation('aabcd'), false, desc);
+
+desc = 'no permutation with even number of chars';
+assertEqual(hasPalindromePermutation('aabbcd'), false, desc);
+
+desc = 'empty string';
+assertEqual(hasPalindromePermutation(''), true, desc);
+
+desc = 'one character string ';
+assertEqual(hasPalindromePermutation('a'), true, desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
