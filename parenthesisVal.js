608e0efb38209b2bfe06d12157fe91c547903f7a
Anthony Lee <ahjlee74@gmail.com>
2021-05-14 16:23:23 -0700

    5/14/21

---
+++ parenthesisVal.js
@@ -0,0 +1,15 @@
+var isValid = function(s) {
+    
+  const hashmap = {
+          "}" : "{",
+          "]" : "[",
+          ")" : "("
+      }
+      const stack = []
+      for(c of s) {
+          if(stack.length && hashmap[c] === stack[stack.length - 1]) stack.pop()
+          else stack.push(c)
+      }
+      return !stack.length
+      
+  };
