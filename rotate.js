125ff25442d7a5100f5067c805c7a8a4c60146bb
Anthony Lee <ahjlee74@gmail.com>
2021-05-18 15:34:30 -0700

    r

---
+++ rotate.js
@@ -0,0 +1,25 @@
+function rotate(nums, k) {
+  if (!nums.length) return [];
+
+  if (k > 0) {
+    k = k % nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+  }
+
+  return nums;
+};
+
+function reverse(arr, start, end) {
+  if (!arr || !arr.length || start >= end) return;
+
+  while (start < end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++, end--;
+  }
+
+  return arr;
+}
