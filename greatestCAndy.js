6f1caa832962eeea046b1b2003e3b79d50d0b381
Anthony Lee <ahjlee74@gmail.com>
2021-05-21 22:34:30 -0700

    candy

---
+++ greatestCAndy.js
@@ -0,0 +1,18 @@
+var kidsWithCandies = function(candies, extraCandies) {
+  let max = 0;
+  let result = [];
+  for (let i = 0; i < candies.length; i++) {
+      max = Math.max(max, candies[i]);
+  }
+  
+  for (let i = 0; i < candies.length; i++) {
+      if (candies[i]+ extraCandies >= max) {
+          result.push(true)
+      } else {
+          result.push(false)
+      }
+  }
+  
+  return result;
+  
+};
