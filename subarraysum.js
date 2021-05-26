efd64b901bf0490b1ed4cc4b16a424abc9ef6ddc
Anthony Lee <ahjlee74@gmail.com>
2021-05-26 11:57:35 -0700

    subarraysum

---
+++ subarraysum.js
@@ -0,0 +1,16 @@
+var subarraySum = function(nums, k) {
+  let sum = 0; 
+  let hash = new Map()
+  hash.set(0,1)
+  let count = 0
+  
+  for(let num of nums){
+    sum += num
+    if(hash.has(sum - k)){
+      count += hash.get(sum-k)
+    }
+    hash.set(sum, (hash.get(sum) || 0) + 1)
+  }
+  
+  return count
+};
